9b24104ad99dc8c0c4f44e1a816357cc3b5d5bf1
andrew erickson and cam hering <andrew+cam@>
2016-10-05 20:53:25 -0700

    initial upload

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+var translate = function(words) {
+  var result = [];
+  var endWord = "ay";
+  if (consonantOrVowel(words)) {
+    result = words.concat(endWord);
+    return result;
+  } else {
+    var firstLetter = words[0];
+    result.push(words.slice(1), firstLetter, endWord);
+    return result.join('');
+  }
+};
+
+var consonantOrVowel = function(words) {
+  var vowels = ["a","e","i","o","u"];
+
+  for (var i = 0; i <= vowels.length; i += 1 ) {
+    if (words[0] === vowels[i]) {
+    return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+$(document).ready(function() {
+  $("form#pigLatin").submit(function(event) {
+    var words = ($("input#words").val());
+    var result = translate(words);
+    $("#result").text(result);
+
+    event.preventDefault();
+  });
+});
